63b76df294ef0726b1806520461c5845bc2df116
gitzzy <tyagidevansh10@gmail.com>
2025-09-27 14:13:46 +0530

    cors

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ mongoose.connect(mongoURL, {
 .catch((err) => console.error('MongoDB connection error:', err));
 
 // Middleware
-app.use(cors()); // For all origins
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend URL
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
