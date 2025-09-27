a858d43d2334ed68fed41e879e03489717634f68
gitzzy <tyagidevansh10@gmail.com>
2025-09-27 13:46:49 +0530

    .

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/' , (req,res) => {
+  res.send('<h1>Backend</h1>')
+})
+
 // Schema and Model
 const userSchema = mongoose.Schema({
   firstName: { type: String, required: true },
